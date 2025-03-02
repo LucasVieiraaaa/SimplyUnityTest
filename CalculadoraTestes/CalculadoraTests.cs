bfe832f818230e7c596f3a6f9f015a16440595e3
LucasVieiraaaa <lucasvieirasouzaa@gmail.com>
2025-03-02 15:08:41 -0300

    Criando Primeiro teste unitario

--- CalculadoraTestes/CalculadoraTests.cs
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -1,15 +1,26 @@
-using System;
-using Calculadora;
+using Calculadora.Services;
 
 namespace CalculadoraTestes;
 public class CalculadoraTests
 {
+    private CalculadoraImp _calc;
+    
+    public CalculadoraTests()
+    {
+        _calc = new CalculadoraImp();
+    }
 
     [Fact]
     public void DeveRetornarASomaDeDoisValoresInteiros()
     {
         //Arange
+        int num1 = 5;
+        int num2 = 10;
+
         //Act
+        int resultado = _calc.Somar(num1,num2);
+
         //Assert
+        Assert. Equal(15,resultado);
     }
 }
